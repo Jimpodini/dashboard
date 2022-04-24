06fc1a0d8022736b827e2b40f79a75341bde52d0
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-04-24 13:48:47 +0200

    Make linear chart prettier

--- src/app/core/charts/linear-chart/linear-chart.component.ts
+++ src/app/core/charts/linear-chart/linear-chart.component.ts
@@ -1,5 +1,6 @@
+// eslint-disable-next-line max-classes-per-file
 import { Component } from '@angular/core';
-import Chart from 'chart.js/auto';
+import Chart, { LineController } from 'chart.js/auto';
 
 @Component({
   selector: 'app-linear-chart',
@@ -18,6 +19,10 @@ export default class LinearChartComponent {
 
     this.ctx = <CanvasRenderingContext2D>this.canvas.getContext('2d');
 
+    const gradientBg = this.ctx.createLinearGradient(0, 0, 0, 400);
+    gradientBg.addColorStop(0, '#f5b6da');
+    gradientBg.addColorStop(1, 'white');
+
     this.myChart = new Chart(this.ctx, {
       type: 'line',
       data: {
@@ -25,14 +30,53 @@ export default class LinearChartComponent {
         datasets: [
           {
             label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [65, 59, 80, 81, 56],
+            borderColor: '#ef0078',
+            tension: 0.4,
+            pointRadius: 2,
             fill: true,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
+            backgroundColor: gradientBg,
+            pointBackgroundColor: '#ef0078',
           },
         ],
       },
-      options: {},
+      options: {
+        scales: {
+          x: {
+            display: false,
+          },
+          y: {
+            display: false,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
     });
   }
 }
+
+// class Custom extends LineController {
+//   override draw() {
+//     // Call bubble controller method to draw all the points
+//     // eslint-disable-next-line prefer-rest-params
+//     super.draw(arguments);
+
+//     // Now we can do some custom drawing for this dataset. Here we'll draw a red box around the first point in each dataset
+//     const meta = this.getMeta();
+//     const pt0 = meta.data[0];
+
+//     const { x, y } = pt0.getProps(['x', 'y']);
+//     const { radius } = pt0.options;
+
+//     const { ctx } = this.chart;
+//     ctx.save();
+//     ctx.strokeStyle = 'red';
+//     ctx.lineWidth = 1;
+//     ctx.strokeRect(x - radius, y - radius, 2 * radius, 2 * radius);
+//     ctx.restore();
+//   }
+// }
