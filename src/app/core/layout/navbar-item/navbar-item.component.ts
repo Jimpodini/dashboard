0dd2d4482360bb7fd2a3a71e3e0fc482e716ff71
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-04-27 07:59:41 +0200

    Change navbar-item stylesheet to sass and use custom theme color

--- src/app/core/layout/navbar-item/navbar-item.component.ts
+++ src/app/core/layout/navbar-item/navbar-item.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-navbar-item',
   templateUrl: './navbar-item.component.html',
-  styleUrls: ['./navbar-item.component.css'],
+  styleUrls: ['./navbar-item.component.scss'],
 })
 export default class NavbarItemComponent {
   @Input() matIcon: string = '';
