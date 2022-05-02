0636c7fff9c19048c68c175125400d3f3234c019
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-02 17:33:08 +0200

    Add distinctUntilChanged pipe to theme observable

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { distinctUntilChanged, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,10 +7,14 @@ import { Subject } from 'rxjs';
 export default class ThemeService {
   private themeSubject = new Subject<Theme>();
 
-  themeObservable = this.themeSubject.asObservable();
+  themeObservable$ = this.themeSubject.asObservable();
 
   switchTheme(theme: Theme) {
     this.themeSubject.next(theme);
+  }
+
+  getThemeObservable(): Observable<Theme> {
+    return this.themeObservable$.pipe(distinctUntilChanged());
   }
 }
 
