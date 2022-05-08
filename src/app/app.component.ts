e235b50581ae51ea77290b425ee4b71a568edfd2
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-08 13:20:26 +0200

    Move dark mode logic to app root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import ThemeService from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export default class AppComponent {}
+export default class AppComponent {
+  @HostBinding('class.darkMode') darkMode: boolean = false;
+
+  constructor(public themeService: ThemeService) {
+    this.themeService.getThemeObservable().subscribe((mode) => {
+      if (mode === 'dark') {
+        this.darkMode = true;
+      } else {
+        this.darkMode = false;
+      }
+    });
+  }
+}
