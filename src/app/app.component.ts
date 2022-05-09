8487de8f84a9a7f49344bf9a4cdb0f5483239f15
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-09 07:51:20 +0200

    Implement dark mode also on angular material dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, HostBinding } from '@angular/core';
 import ThemeService from './services/theme.service';
 
@@ -9,12 +10,19 @@ import ThemeService from './services/theme.service';
 export default class AppComponent {
   @HostBinding('class.darkMode') darkMode: boolean = false;
 
-  constructor(public themeService: ThemeService) {
+  constructor(
+    public themeService: ThemeService,
+    private overlayContainer: OverlayContainer
+  ) {
     this.themeService.getThemeObservable().subscribe((mode) => {
       if (mode === 'dark') {
         this.darkMode = true;
+        this.overlayContainer.getContainerElement().classList.add('darkMode');
       } else {
         this.darkMode = false;
+        this.overlayContainer
+          .getContainerElement()
+          .classList.remove('darkMode');
       }
     });
   }
