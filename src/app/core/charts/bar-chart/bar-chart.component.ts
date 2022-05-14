0b3bd42434420b17e76bb046976eab00b6ecd602
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-14 14:17:40 +0200

    Add code preview to bar chart

--- src/app/core/charts/bar-chart/bar-chart.component.ts
+++ src/app/core/charts/bar-chart/bar-chart.component.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line max-classes-per-file
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import Chart, { ChartConfiguration } from 'chart.js/auto';
 import { Subscription } from 'rxjs';
 import ThemeService, { Theme } from 'src/app/services/theme.service';
+import CodePreviewComponent from '../../code-preview/code-preview.component';
 import { DashboardChart } from '../dashboard-chart.interface';
 
 @Component({
@@ -25,7 +27,63 @@ export default class BarChartComponent
 
   s1: Subscription;
 
-  constructor(private themeService: ThemeService) {}
+  code = `
+    this.canvas = <HTMLCanvasElement>document.getElementById('myBarChart');
+
+    this.ctx = <CanvasRenderingContext2D>this.canvas.getContext('2d');
+
+    const data = [3, 5, 4, 8, 10];
+    this.latestData = data[data.length - 1];
+
+    this.config = {
+      type: 'bar',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [
+          {
+            label: 'Transactions',
+            data,
+            borderColor: '#ef0078',
+            backgroundColor: '#ef0078',
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            display: false,
+          },
+          y: {
+            display: false,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    };
+
+    this.myChart = new Chart(this.ctx, this.config);
+
+    this.s1 = this.themeService.getThemeObservable().subscribe((theme) => {
+      this.setThemeColor(theme);
+    });
+
+    setThemeColor(theme: Theme): void {
+      if (theme === 'dark') {
+        this.config.data.datasets[0].backgroundColor = '#ff9500';
+        this.config.data.datasets[0].borderColor = '#ff9500';
+      } else {
+        this.config.data.datasets[0].backgroundColor = '#ef0078';
+        this.config.data.datasets[0].borderColor = '#ef0078';
+      }
+      this.myChart?.update();
+    }
+    `;
+
+  constructor(private themeService: ThemeService, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.canvas = <HTMLCanvasElement>document.getElementById('myBarChart');
@@ -81,6 +139,15 @@ export default class BarChartComponent
       this.config.data.datasets[0].borderColor = '#ef0078';
     }
     this.myChart?.update();
+  }
+
+  openDialog() {
+    this.dialog.open(CodePreviewComponent, {
+      height: '80vh',
+      data: {
+        code: this.code,
+      },
+    });
   }
 
   ngOnDestroy() {
