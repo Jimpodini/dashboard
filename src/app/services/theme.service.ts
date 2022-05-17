81255a8db6c3b9d78ceae4b47d52e5b81423605e
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-17 08:08:31 +0200

    Save selected theme colors in css variables so that they can be fetched from js and then used in chartjs initialization

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -19,8 +19,13 @@ export default class ThemeService {
 
   themeObservable$ = this.themeSubject.asObservable();
 
+  primaryColors: ThemeColors = {};
+
+  primaryDarkColors: ThemeColors = {};
+
   constructor(private hljsLoader: HighlightLoader) {
     this.hljsLoader.setTheme(themeGithub);
+    this.setThemeColors();
   }
 
   switchTheme(theme: Theme) {
@@ -31,6 +36,56 @@ export default class ThemeService {
   getThemeObservable(): Observable<Theme> {
     return this.themeObservable$.pipe(distinctUntilChanged(), shareReplay(1));
   }
+
+  private setThemeColors(): void {
+    this.primaryColors.primary50 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--primary-50');
+    this.primaryColors.primary300 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--primary-300');
+    this.primaryColors.primary500 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--primary-500');
+    this.primaryColors.primary700 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--primary-700');
+    this.primaryColors.primary900 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--primary-900');
+    this.primaryColors.cardBackground = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--card-background');
+
+    this.primaryDarkColors.primary300 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-primary-300');
+    this.primaryDarkColors.primary500 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-primary-500');
+    this.primaryDarkColors.primary700 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-primary-700');
+    this.primaryDarkColors.primary900 = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-primary-900');
+    this.primaryDarkColors.cardBackground = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-card-background');
+    this.primaryDarkColors.background = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--dark-background');
+  }
 }
 
 export type Theme = 'dark' | 'light';
+
+export type ThemeColors = {
+  primary50?: string;
+  primary300?: string;
+  primary500?: string;
+  primary700?: string;
+  primary900?: string;
+  cardBackground?: string;
+  background?: string;
+};
